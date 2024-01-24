372c3b9d40ff30cc22dcb8ed101806e6191b05a5
jal119 <105179174+jal119@users.noreply.github.com>
2024-01-24 15:03:46 -0600

    Update backend.tf

--- Jenkins-Server-TF/backend.tf
+++ Jenkins-Server-TF/backend.tf
@@ -1,10 +1,8 @@
 terraform {
   backend "s3" {
-    bucket         = "my-ews-baket1"
+    bucket         = "ayan-general288882"
     region         = "us-east-1"
     key            = "End-to-End-Kubernetes-Three-Tier-DevSecOps-Project/Jenkins-Server-TF/terraform.tfstate"
-    dynamodb_table = "Lock-Files"
-    encrypt        = true
   }
   required_version = ">=0.13.0"
   required_providers {
